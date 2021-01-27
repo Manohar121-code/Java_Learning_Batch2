451bc585db9041644a5466cb1007fbeb45c4abb5
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2021-01-27 20:13:05 +0530

    data types

---
+++ src/basics/dataTypes/DataTypes.java
@@ -0,0 +1,42 @@
+package basics.dataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		byte b = 1;
+		System.out.println("byte value - "+b);
+		
+		char ch = 'g';
+		System.out.println("char value - "+ch);
+		
+		short s = 15;
+		System.out.println("short value - "+s);
+	
+		int i = 10;
+		System.out.println("int value - "+i);
+		
+		long l = 2020;
+		System.out.println("long value - "+l);
+		
+		float f = 12.34f;
+		System.out.println("float value - "+f);
+		
+		double d = 3827.878;
+		System.out.println("double value - "+d);
+		
+		boolean bool = true;
+		System.out.println("boolean value - "+bool);
+		
+		String str = "java is a programming language";
+		System.out.println("String value - "+str);
+		
+		int myFirstLetterOfPhoneNumber = 7;
+		
+		int n1 = 10;
+		int n2 = 20;
+		System.out.println("sum - "+ (n1+n2));
+	
+	}
+
+}
